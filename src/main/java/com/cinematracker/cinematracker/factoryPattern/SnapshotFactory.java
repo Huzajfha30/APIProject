94d8b53087d6c37dd11d1dd937a93f7283f392a0
Huzajfha Bayoumi <hubay22@student.sdu.dk>
2025-05-13 22:18:17 +0200

    some fixes

--- src/main/java/com/cinematracker/cinematracker/factoryPattern/SnapshotFactory.java
+++ src/main/java/com/cinematracker/cinematracker/factoryPattern/SnapshotFactory.java
@@ -31,11 +31,6 @@ public class SnapshotFactory {
         snapshots.setCreatedAt(createdAt);
         return snapshots;
     }
-    public Snapshots createDefaultSnapshot() {
-        LocalDateTime now = LocalDateTime.now();
-        Snapshots snapshots = new Snapshots();
-        snapshots.setCreatedAt(now);
-        return snapshots;
     }
 
 
